ea22173c796f39a8dbac159f770b4cb37ac5cc88
Gabriel Yasuhiro Zoppello Kanashiro <gyasuhir@c2r8p5.42sp.org.br>
2024-10-06 18:21:05 -0300

    add first files

---
+++ piscine-reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyasuhir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 15:31:38 by gyasuhir          #+#    #+#             */
+/*   Updated: 2024/10/06 15:45:31 by gyasuhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return ;
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+	return ;
+}
---
+++ piscine-reloaded/ex07/ft_print_numbers.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyasuhir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 15:44:35 by gyasuhir          #+#    #+#             */
+/*   Updated: 2024/10/06 15:48:08 by gyasuhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return ;
+}
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+	return ;
+}
